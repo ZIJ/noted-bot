c177db6e9d381bf54f09bb74c2e97d94ca1638d6
Igor Zalutski <izalutski@gmail.com>
2022-02-19 23:02:09 +0000

    Add healthcheck

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 import { Telegraf } from 'telegraf';
 import { Client } from '@notionhq/client';
+import { express } from 'express';
 
 const { BOT_TOKEN, NOTION_TOKEN, NOTION_ROOT } = process.env;
 
 const bot = new Telegraf(BOT_TOKEN);
 const notion = new Client({ auth: NOTION_TOKEN });
+const server = express();
+
+// health check
+server.get('/', (req, res) => {
+  res.send('Noted Bot - OK');
+});
+
+server.listen(8080);
 
 function pageTitle(text) {
   const maxLength = 35;
