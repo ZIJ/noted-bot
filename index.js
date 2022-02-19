e9192298212a2e11d1eed63caa0e929ba04f91bb
Igor Zalutski <izalutski@gmail.com>
2022-02-19 15:30:57 +0000

    Add launch

--- index.js
+++ index.js
@@ -6,5 +6,7 @@ bot.command('helloworld', (ctx) => {
   ctx.reply('Hello World!');
 });
 
+bot.launch();
+
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
