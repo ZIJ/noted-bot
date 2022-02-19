beb2d49baf0c4ca7680b6d2dc0ddbe9fa5a774bf
Igor Zalutski <izalutski@gmail.com>
2022-02-19 20:58:23 +0000

    Integrate Notion API

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
-const { Telegraf } = require('telegraf');
+import { Telegraf } from 'telegraf';
+import { Client } from '@notionhq/client';
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
+const { BOT_TOKEN, NOTION_TOKEN, NOTION_ROOT } = process.env;
+
+const bot = new Telegraf(BOT_TOKEN);
+const notion = new Client({ auth: NOTION_TOKEN });
+
+function makePageCreateData(text) {
+  return {
+    parent: {
+      page_id: NOTION_ROOT,
+    },
+    properties: {
+      title: [{
+        text: {
+          content: text,
+        },
+      }],
+    },
+  };
+}
 
 bot.command('helloworld', (ctx) => {
   ctx.reply('Hello World!');
+});
+
+bot.on('message', async (ctx) => {
+  const page = makePageCreateData(ctx.message.text);
+  try {
+    await notion.pages.create(page);
+  } catch (e) {
+    console.error(e);
+  }
 });
 
 bot.launch();
